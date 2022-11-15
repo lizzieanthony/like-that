b00a7f94e8f8f820dce3be9f6964b45dc7f597cd
Lizzie <lizziewrab@me.com>
2022-11-14 20:12:39 -0600

    styling

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const Product = ({product}) => {
     return (  
-        <div className="product">{product.name} {product.description}</div>
+        <ul>
+        <img src={product.image_url} className="productimage" alt="product"/>
+            <div className="productDetails"> 
+            <h1>{product.name}</h1>
+            <br />
+            <h3>{product.description}</h3>
+            <br />
+            <h4>{product.price}</h4>
+            </div>
+        </ul>
+        
     );
 }
  
--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -3,16 +3,16 @@ import { useParams } from "react-router-dom";
 import Product from "./Product";
 
 const ProductDetails = () => {
-const {productId} = useParams();
+const {id} = useParams();
 const [currentProduct, setCurrentProduct] = useState("");
 
     useEffect(() => {
-        fetch(`/products/${productId}`)
+        fetch(`/products/${id}`)
         .then((r) => r.json())
         .then((p) => {
             setCurrentProduct(p)
         });
-    }, [productId])
+    }, [id])
 
     return (  
         <div>
