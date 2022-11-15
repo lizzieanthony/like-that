05b584ca88fa23b217810148f1ad73726b870cfa
Lizzie <lizziewrab@me.com>
2022-11-15 16:59:14 -0600

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # resources :reviews, only: [:create, :update, :destroy]
   # resources :users
   resources :products, only: [:index, :show, :create] do 
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy" 
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
